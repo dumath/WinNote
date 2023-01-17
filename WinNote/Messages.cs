0b9808653b570f2cdaa16e5578198e473f8b1288
dumath <34304654+dumath@users.noreply.github.com>
2023-01-17 17:11:48 +0300

    .

--- WinNote/Messages.cs
+++ WinNote/Messages.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace WinNote
 {
     static class Messages
     {
         /*
          * Порядок.
-         * Чтобы сообщения, к примеру исключений, в разных местах не выглядели по разному, сообщение ошибки описывается тут. */
+         * Чтобы сообщения, к примеру - исключений, в разных местах не выглядели по разному, сообщение ошибки описывается тут. */
         public const string SERVER_NAME_ERROR = "Неверное имя сервера.";
         public const string DB_NAME_ERROR = "Неверное имя базы данных.";
         public const string TIME_OUT_ERROR = "Неверное время ожидания.";
